acb808fa902c338aeb44ed69e4d129315a4c8694
izyanrosli <44498300+izyanrosli@users.noreply.github.com>
2018-12-17 00:00:48 +0800

    Update home.js

--- popular/home.js
+++ popular/home.js
@@ -18,3 +18,14 @@ window.onclick = function(event) {
     }
   }
 }
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("header").style.top = "0";
+  } else {
+    document.getElementById("header").style.top = "-150px";
+  }
+  prevScrollpos = currentScrollPos;
+}
