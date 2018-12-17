54e66cfea26645485017310dece50c84b0e23af2
izyanrosli <44498300+izyanrosli@users.noreply.github.com>
2018-12-17 20:14:04 +0800

    Add files via upload

---
+++ popular/like.js
@@ -0,0 +1,77 @@
+
+var likes1 = 21; //global var //admin1
+function like1(){
+document.getElementById("like1").innerHTML = likes1;
+likes1 += 1;
+}
+
+var dislikes1 = 2; //global var //admin1
+function dislike1(){
+document.getElementById("dislike1").innerHTML = dislikes1;
+dislikes1 += 1;
+}
+
+
+var likes2 = 13; //global var //admin1
+function like2(){
+document.getElementById("like2").innerHTML = likes2;
+likes2 += 1;
+}
+
+var dislikes2 = 1; //global var //admin1
+function dislike2(){
+document.getElementById("dislike2").innerHTML = dislikes2;
+dislikes2 += 1;
+}
+
+
+var likes3 = 17; //global var //admin1
+function like3(){
+document.getElementById("like3").innerHTML = likes3;
+likes3 += 1;
+}
+
+var dislikes3 = 6; //global var //admin1
+function dislike3(){
+document.getElementById("dislike3").innerHTML = dislikes3;
+dislikes3 += 1;
+}
+
+
+var likes4 = 4; //global var //admin1
+function like4(){
+document.getElementById("like4").innerHTML = likes4;
+likes4 += 1;
+}
+
+var dislikes4 = 0; //global var //admin1
+function dislike4(){
+document.getElementById("dislike4").innerHTML = dislikes4;
+dislikes4 += 1;
+}
+
+
+var likes5 = 19; //global var //admin1
+function like5(){
+document.getElementById("like5").innerHTML = likes5;
+likes5 += 1;
+}
+
+var dislikes5 = 8; //global var //admin1
+function dislike5(){
+document.getElementById("dislike5").innerHTML = dislikes5;
+dislikes5 += 1;
+}
+
+
+var likes6 = 22; //global var //admin1
+function like6(){
+document.getElementById("like6").innerHTML = likes6;
+likes6 += 1;
+}
+
+var dislikes6 = 5; //global var //admin1
+function dislike6(){
+document.getElementById("dislike6").innerHTML = dislikes6;
+dislikes6 += 1;
+}
